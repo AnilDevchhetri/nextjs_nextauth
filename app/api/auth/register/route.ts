47bcd38c80261b31a8eb38aa11d8f3547f97eef8
AnilDevchhetri <chhetrivision@gmail.com>
2026-03-14 23:00:43 +0900

    1:20

---
+++ app/api/auth/register/route.ts
@@ -0,0 +1,39 @@
+import { connectToDatabase } from "@/lib/db";
+import User from "@/models/User";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+  try {
+    const { email, password } = await request.json();
+    if (!email || !password) {
+      return NextResponse.json(
+        { error: "Email and passwrod are requried" },
+        { status: 400 },
+      );
+    }
+    await connectToDatabase();
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return NextResponse.json(
+        {
+          error: "User already registered",
+        },
+        { status: 400 },
+      );
+    }
+    await User.create({
+      email,
+      password,
+    });
+    return NextResponse.json(
+      { message: "User register successfull" },
+      { status: 400 },
+    );
+  } catch (error) {
+    console.error("Register error: ", error);
+    return NextResponse.json(
+      { error: "Faild to register user" },
+      { status: 400 },
+    );
+  }
+}
